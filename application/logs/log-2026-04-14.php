388d78f73b9c378177a74a263be321b43f67b747
Rizeky Gunawan <muh.rizeky@gmail.com>
2026-04-14 10:25:03 +0700

    Refactor: Extract inline CSS in view into separate assets file

--- application/logs/log-2026-04-14.php
+++ application/logs/log-2026-04-14.php
@@ -7,3 +7,10 @@ ERROR - 2026-04-14 01:54:37 --> 404 Page Not Found: Dist/img
 ERROR - 2026-04-14 01:54:37 --> 404 Page Not Found: Dist/img
 ERROR - 2026-04-14 01:55:32 --> Severity: Warning --> Invalid argument supplied for foreach() C:\laragon\www\evsakippmk-inspektorat\application\views\v_ev.php 643
 ERROR - 2026-04-14 01:55:33 --> 404 Page Not Found: Dist/img
+ERROR - 2026-04-14 03:16:49 --> 404 Page Not Found: Assets/plugins
+ERROR - 2026-04-14 03:16:49 --> 404 Page Not Found: Assets/plugins
+ERROR - 2026-04-14 03:17:00 --> Severity: Warning --> Invalid argument supplied for foreach() C:\laragon\www\evsakippmk-inspektorat\application\views\templates\header.php 153
+ERROR - 2026-04-14 03:17:00 --> Severity: Warning --> Invalid argument supplied for foreach() C:\laragon\www\evsakippmk-inspektorat\application\views\templates\footer.php 69
+ERROR - 2026-04-14 03:22:17 --> 404 Page Not Found: Dist/img
+ERROR - 2026-04-14 03:22:18 --> 404 Page Not Found: Dist/img
+ERROR - 2026-04-14 03:22:27 --> 404 Page Not Found: Dist/img
